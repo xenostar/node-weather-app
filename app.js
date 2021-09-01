f84a7d1bc1627426971837f1e5ec3834939753e0
xenostar <bryan.a.leewood@gmail.com>
2021-08-31 22:44:08 -0700

    main Adding command line arguments for finding location.

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("Philadelphia", (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+const [locationArg] = process.argv.slice(2);
 
-forecast(44.1545, -75.7088, (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+if (locationArg) {
+  geocode(locationArg, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      console.log(data.location);
+      console.log(forecastData.summary);
+    });
+  });
+} else {
+  console.log("Please provide an address.");
+}
